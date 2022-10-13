8c8f8283bd9357f16c948a67916175df979970c7
philippe.zigler <philippe.zigler@neuland-bfi.de>
2022-10-13 11:09:21 +0200

    new branch - mnf-1 init

--- src/main/java/de/szut/lf8_project/hello/dto/HelloGetDto.java
+++ src/main/java/de/szut/lf8_project/hello/dto/HelloGetDto.java
@@ -2,10 +2,7 @@ package de.szut.lf8_project.hello.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 @AllArgsConstructor
 @Getter
