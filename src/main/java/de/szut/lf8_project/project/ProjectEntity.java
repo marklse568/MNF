0806f42a09f6d2a731f59f14225b562b293859ec
Mark L <marklse568@gmail.com>
2022-11-04 18:42:54 +0100

    create project entity

---
+++ src/main/java/de/szut/lf8_project/project/ProjectEntity.java
@@ -0,0 +1,30 @@
+package de.szut.lf8_project.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "project")
+public class ProjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private long assigneeId;
+    private long clientId;
+    private long clientAssigneeId;
+    private String description;
+    private String comment;
+    private Date plannedEndDate;
+    private Date startDate;
+    private Date endDate;
+}
