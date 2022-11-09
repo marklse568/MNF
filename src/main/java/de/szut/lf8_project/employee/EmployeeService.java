4edb9d09bf4aa937fbebd0ccead770cdc60476d8
philippe.zigler <philippe.zigler@neuland-bfi.de>
2022-11-09 08:40:28 +0100

    deleted old imports

--- src/main/java/de/szut/lf8_project/employee/EmployeeService.java
+++ src/main/java/de/szut/lf8_project/employee/EmployeeService.java
@@ -1,11 +1,8 @@
 package de.szut.lf8_project.employee;
 
-import de.szut.lf8_project.hello.HelloEntity;
-import de.szut.lf8_project.hello.HelloRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Scanner;
 
 @Service
 public class EmployeeService {
