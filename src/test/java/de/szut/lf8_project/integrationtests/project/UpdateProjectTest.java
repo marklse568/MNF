7e43c814de90e3a6c1008afe420e086dc36da31f
Mark L <marklse568@gmail.com>
2022-11-12 16:35:57 +0100

    Refactor tests to use jwt and correct id

--- src/test/java/de/szut/lf8_project/integrationtests/project/UpdateProjectTest.java
+++ src/test/java/de/szut/lf8_project/integrationtests/project/UpdateProjectTest.java
@@ -1,7 +1,7 @@
 package de.szut.lf8_project.integrationtests.project;
 
+import de.szut.lf8_project.integrationtests.BaseIntegrationTest;
 import de.szut.lf8_project.project.ProjectEntity;
-import de.szut.lf8_project.testcontainers.AbstractIntegrationTest;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class UpdateProjectTest extends AbstractIntegrationTest {
+public class UpdateProjectTest extends BaseIntegrationTest {
     @Test
     void updateProject() throws Exception {
         final ProjectEntity project = new ProjectEntity();
@@ -22,20 +22,23 @@ public class UpdateProjectTest extends AbstractIntegrationTest {
         project.setClientId(456);
         project.setClientAssigneeId(789);
         project.setComment("updated successfully");
-        this.projectRepository.save(project);
+        this.projectRepository.saveAndFlush(project);
 
-        String content = "{\n" +
-                "\t\"id\": 1,\n" +
+        long projectId = project.getId();
+
+        String content = String.format("{\n" +
+                "\t\"id\": %d,\n" +
                 "\t\"assigneeId\": 123,\n" +
                 "\t\"clientId\": 456,\n" +
                 "\t\"clientAssigneeId\": 789,\n" +
                 "\t\"comment\": \"updated successfully\"\n" +
-                "}";
+                "}", projectId);
 
         final String contentAsString =
-                this.mockMvc.perform(put("/project").content(content).contentType(MediaType.APPLICATION_JSON))
+                this.mockMvc.perform(put("/project").content(content).contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", generateJwt()))
                         .andExpect(status().is2xxSuccessful())
-                        .andExpect(jsonPath("id", is(1)))
+                        .andExpect(jsonPath("id", is((int) projectId)))
                         .andExpect(jsonPath("assigneeId", is(123)))
                         .andExpect(jsonPath("clientId", is(456)))
                         .andExpect(jsonPath("clientAssigneeId", is(789)))
@@ -47,7 +50,7 @@ public class UpdateProjectTest extends AbstractIntegrationTest {
         final long id = Long.parseLong(new JSONObject(contentAsString).get("id").toString());
 
         final Optional<ProjectEntity> loadedEntity = projectRepository.findById(id);
-        assertThat(loadedEntity.get().getId()).isEqualTo(1);
+        assertThat(loadedEntity.get().getId()).isEqualTo(projectId);
         assertThat(loadedEntity.get().getAssigneeId()).isEqualTo(123);
         assertThat(loadedEntity.get().getClientId()).isEqualTo(456);
         assertThat(loadedEntity.get().getClientAssigneeId()).isEqualTo(789);
